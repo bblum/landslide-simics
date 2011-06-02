0404ab6a2d8c9dca7029dbbae953e33580f68446
Benjamin Blum <bblum@ghc11.ghc.andrew.cmu.edu>
2011-06-02 17:07:50 -0400

    prelim convert hax.c to use snoop_memory

--- work/modules/hax/hax.c
+++ work/modules/hax/hax.c
@@ -1,5 +1,5 @@
 /*
-  empty-device.c - Skeleton code to base new device modules on
+  hax.c - A Module for Simics which provides yon Hax and Sploits
 
   Copyright 1998-2009 Virtutech AB
   
@@ -30,16 +30,16 @@
 #include <simics/alloc.h>
 #include <simics/utils.h>
 
-/* USER-TODO: Set the name of the device class */
-#define DEVICE_NAME "hax"
+#define MODULE_NAME "hax"
 
 typedef struct {
-        /* log_object_t must be the first thing in the device struct */
-        log_object_t log;
+	/* log_object_t must be the first thing in the device struct */
+	log_object_t log;
 
-        /* USER-TODO: Add user specific members here. The 'value' member
-           is only an example to show how to implement an attribute */
-        int value;
+	/* USER-TODO: Add user specific members here. The 'value' member
+	 * is only an example to show how to implement an attribute */
+	int value;
+	int hax_count;
 } hax_t;
 
 /*
@@ -47,83 +47,88 @@ typedef struct {
  * call (see init_local() below), and is used as a constructor for every
  * instance of the hax class.
  */
-static conf_object_t *
-new_instance(parse_object_t *parse_obj)
+static conf_object_t *new_instance(parse_object_t *parse_obj)
 {
-        hax_t *empty = MM_ZALLOC(1, hax_t);
-        SIM_log_constructor(&empty->log, parse_obj);
+	hax_t *empty = MM_ZALLOC(1, hax_t);
+	SIM_log_constructor(&empty->log, parse_obj);
 
-        /* USER-TODO: Add initialization code for new instances here */
-        empty->value = 31337;
+	/* USER-TODO: Add initialization code for new instances here */
+	empty->value = 31337;
 
-        return &empty->log.obj;
+	empty->hax_count = 0;
+
+	return &empty->log.obj;
 }
 
-static exception_type_t
-operation(conf_object_t *obj, generic_transaction_t *mop, map_info_t info)
+
+static cycles_t hax_main(conf_object_t *obj, conf_object_t *space,
+			 map_list_t *map, generic_transaction_t *mem_op)
 {
-        hax_t *empty = (hax_t *)obj;
-        int offset = (int)(SIM_get_mem_op_physical_address(mop)
-                           + info.start - info.base);
+	hax_t *h = (hax_t *)obj;
+	int offset = (int)(SIM_get_mem_op_physical_address(mem_op));
 
-        /* USER-TODO: Handle accesses to the device here */
-
-        if (SIM_mem_op_is_read(mop)) {
-                SIM_log_info(2, &empty->log, 0, "read from offset %d", offset);
-                SIM_set_mem_op_value_le(mop, 0);
-        } else {
-                SIM_log_info(2, &empty->log, 0, "write to offset %d", offset);
-        }
-        return Sim_PE_No_Exception;
+	h->hax_count++;
+	if (h->hax_count % 1000 == 0) {
+		printf("hax number %d at offset %d\n", h->hax_count, offset);
+	}
+	/* USER-TODO: Handle accesses to the device here */
+#if 0
+	if (SIM_mem_op_is_read(mop)) {
+		SIM_log_info(2, &empty->log, 0, "read from offset %d", offset);
+		SIM_set_mem_op_value_le(mop, 0);
+	} else {
+		SIM_log_info(2, &empty->log, 0, "write to offset %d", offset);
+	}
+#endif
+	return 0;
 }
 
-static set_error_t
-set_value_attribute(void *arg, conf_object_t *obj,
-                    attr_value_t *val, attr_value_t *idx)
+static set_error_t set_value_attribute(void *arg, conf_object_t *obj,
+				       attr_value_t *val, attr_value_t *idx)
 {
-        hax_t *empty = (hax_t *)obj;
-        empty->value = SIM_attr_integer(*val);
-        return Sim_Set_Ok;
+	hax_t *empty = (hax_t *)obj;
+	empty->value = SIM_attr_integer(*val);
+	return Sim_Set_Ok;
 }
 
-static attr_value_t
-get_value_attribute(void *arg, conf_object_t *obj, attr_value_t *idx)
+static attr_value_t get_value_attribute(void *arg, conf_object_t *obj,
+					attr_value_t *idx)
 {
-        hax_t *empty = (hax_t *)obj;
-        return SIM_make_attr_integer(empty->value);
+	hax_t *empty = (hax_t *)obj;
+	return SIM_make_attr_integer(empty->value);
 }
 
 /* init_local() is called once when the device module is loaded into Simics */
-void
-init_local(void)
+void init_local(void)
 {
-        printf("hax init called\n");
+	printf("welcome to hax.\n");
 
-        const class_data_t funcs = {
-                .new_instance = new_instance,
-                .class_desc = "C device template",
-                .description =
-                "This is a descriptive description describing the "
-                DEVICE_NAME " class descriptively."
-        };
+	const class_data_t funcs = {
+		.new_instance = new_instance,
+		.class_desc = "hax and sploits",
+		.description = "here we have a simix module which provides not"
+			" only hax or sploits individually but rather a great"
+			" conjunction of the two."
+	};
 
-        /* Register the empty device class. */
-        conf_class_t *conf_class = SIM_register_class(DEVICE_NAME, &funcs);
+	/* Register the empty device class. */
+	conf_class_t *conf_class = SIM_register_class(MODULE_NAME, &funcs);
 
-        /* Register the 'io_memory' interface, that is used to implement
-           memory mapped accesses */
-        static const io_memory_interface_t memory_iface = {
-                .operation = operation
-        };
-        SIM_register_interface(conf_class, IO_MEMORY_INTERFACE, &memory_iface);
+	/* Register the 'io_memory' interface, that is used to implement
+	 * memory mapped accesses */
+	static const snoop_memory_interface_t memory_iface = {
+		.operate = hax_main
+	};
+	SIM_register_interface(conf_class, SNOOP_MEMORY_INTERFACE,
+			       &memory_iface);
 
-        /* USER-TODO: Add any attributes for the device here */
+	/* USER-TODO: Add any attributes for the device here */
 
-        SIM_register_typed_attribute(
-                conf_class, "value",
-                get_value_attribute, NULL,
-                set_value_attribute, NULL,
-                Sim_Attr_Optional,
-                "i", NULL,
-                "Value containing a valid valuable valuation.");
+	SIM_register_typed_attribute(
+		conf_class, "value",
+		get_value_attribute, NULL,
+		set_value_attribute, NULL,
+		Sim_Attr_Optional,
+		"i", NULL,
+		"Value containing a valid valuable valuation.");
 }
