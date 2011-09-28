042309d631d10ec1517a9e64c13c0999ae774026
Ben Blum <bblum@andrew.cmu.edu>
2011-09-28 16:24:51 -0400

    add stub explore.c

---
+++ work/modules/landslide/explore.c
@@ -0,0 +1,13 @@
+/**
+ * @file explore.c
+ * @brief choice tree exploration
+ * @author Ben Blum <bbum@andrew.cmu.edu>
+ */
+
+#include "tree.h"
+
+struct hax *explore(struct hax *current, struct hax *root, int *new_tid)
+{
+	return NULL;
+}
+
