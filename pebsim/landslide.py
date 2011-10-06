1c625e6252c5a80b92981d83a2c273b9db4e83bd
Ben Blum <bblum@andrew.cmu.edu>
2011-10-06 12:41:30 -0400

    improve landslide.py wrapper for better debug

--- pebsim/landslide.py
+++ pebsim/landslide.py
@@ -1,6 +1,9 @@
+import os
+
 # TODO: factor out this string
 cmd_file = "./landslide-save.simics"
 
-while True:
-	SIM_continue(0)
+SIM_continue(0)
+while os.path.isfile(cmd_file):
 	SIM_run_command_file(cmd_file, False)
+	SIM_continue(0)
