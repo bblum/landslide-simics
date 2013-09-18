5335a429162f0e2e33b82d293111f18771dcfa70
Ben Blum <bblum@andrew.cmu.edu>
2013-09-18 15:39:12 -0400

    Prepare-workspace.sh feedback on error/success.

--- prepare-workspace.sh
+++ prepare-workspace.sh
@@ -5,7 +5,14 @@ if [ ! -d work -o ! -d pebsim ]; then
         exit 1
 fi
 
+function die() {
+	echo "error $1"
+	exit 1
+}
 
-sed -i "s:SIMICS_WORKSPACE=.*:SIMICS_WORKSPACE=$PWD/work:" work/config.mk
-sed -i "s:add-module-directory.*:add-module-directory $PWD/work/amd64-linux/lib:" pebsim/config.simics
-sed -i "s:SIMENV=/.*:SIMENV=$PWD/pebsim:" pebsim/simics4
+sed -i "s:SIMICS_WORKSPACE=.*:SIMICS_WORKSPACE=$PWD/work:" work/config.mk || die 1
+sed -i "s:add-module-directory.*:add-module-directory $PWD/work/amd64-linux/lib:" pebsim/config.simics || die 2
+sed -i "s:SIMENV=/.*:SIMENV=$PWD/pebsim:" pebsim/simics4 || die 3
+
+echo "Success."
+exit 0
