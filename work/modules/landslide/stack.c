1752be2afdad29f7c81c728de8af2ceaf2eb584d
Ben Blum <bblum@andrew.cmu.edu>
2014-09-11 14:51:06 -0400

    Fix buffer overflow. Close #98

--- work/modules/landslide/stack.c
+++ work/modules/landslide/stack.c
@@ -81,7 +81,7 @@ void print_stack_trace(verbosity v, struct stack_trace *st)
 /* Prints a stack trace to a multiline html table. Returns length printed. */
 unsigned int html_stack_trace(char *buf, unsigned int maxlen, struct stack_trace *st)
 {
-#define PRINT(...) do { pos += scnprintf(buf + pos, maxlen, __VA_ARGS__); } while (0)
+#define PRINT(...) do { pos += scnprintf(buf + pos, maxlen - pos, __VA_ARGS__); } while (0)
 	unsigned int pos = 0;
 	bool first_frame = true;
 	struct stack_frame *f;
