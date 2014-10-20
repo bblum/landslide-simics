9aea3cfed51281509297cd803ce5bdaf1c932839
Ben Blum <bblum@andrew.cmu.edu>
2014-10-20 18:37:45 -0400

    oops that buffer needs to be longer

--- work/modules/landslide/lockset.c
+++ work/modules/landslide/lockset.c
@@ -142,8 +142,8 @@ void lockset_remove(struct sched_state *s, int lock_addr, enum lock_type type,
 	if (_lockset_remove(LOCKSET_OF(s->cur_agent, in_kernel), lock_addr, type))
 		return;
 
-	char lock_name[32];
-	symtable_lookup_data(lock_name, 32, lock_addr);
+	char lock_name[BUF_SIZE];
+	symtable_lookup_data(lock_name, BUF_SIZE, lock_addr);
 	lsprintf(ALWAYS, COLOUR_BOLD COLOUR_YELLOW "WARNING: Lock handoff with "
 		 "TID %d unlocking %s @ 0x%x; expect data race tracking may be "
 		 "incorrect\n" COLOUR_DEFAULT, s->cur_agent->tid, lock_name, lock_addr);
