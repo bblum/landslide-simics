bblum/landslide-simics

commit a0d9dd7bf9e39c665f9a4210e11cee3a3ef6f89e

Use ls->instruction_text to optimize disassembly of current instruction instead of READ_MEMORY every instruction. Measured 25% speedup. Close #184

work/modules/landslide/user_specifics.c
@@ -54,17 +54,14 @@ bool user_report_end_fail(conf_object_t *cpu, unsigned int eip)
 #endif
 }
 
-bool user_yielding(conf_object_t *cpu, unsigned int eip)
+bool user_yielding(struct ls_state *ls)
 {
 #ifdef USER_YIELD_ENTER
-#if 0
-	return eip == USER_YIELD_ENTER;
-#else
 	/* Special case frumious logic. Compare to check_user_syscall(). Handles
 	 * p2s with assembly yield invocations (sounds like a WISE IDEA). */
-	return USER_MEMORY(eip) && READ_BYTE(cpu, eip) == OPCODE_INT &&
-		OPCODE_INT_ARG(cpu, eip) == YIELD_INT;
-#endif
+	return USER_MEMORY(ls->eip) &&
+		ls->instruction_text[0] == OPCODE_INT &&
+		ls->instruction_text[1] == YIELD_INT;
 #else
 	return false;
 #endif

work/modules/landslide/arbiter.c
@@ -129,7 +129,7 @@ bool arbiter_interested(struct ls_state *ls, bool just_finished_reschedule,
 		*voluntary = true;
 		return true;
 	/* is the kernel idling, e.g. waiting for keyboard input? */
-	} else if (READ_BYTE(ls->cpu0, ls->eip) == OPCODE_HLT) {
+	} else if (ls->instruction_text[0] == OPCODE_HLT) {
 		lskprintf(INFO, "What are you waiting for? (HLT state)\n");
 		*need_handle_sleep = true;
 		ASSERT_ONE_THREAD_PER_PP(ls);
@@ -142,7 +142,7 @@ bool arbiter_interested(struct ls_state *ls, bool just_finished_reschedule,
 	/* check for data races */
 	} else if (suspected_data_race(ls)) {
 		// FIXME: #88
-		assert(!instruction_is_atomic_swap(ls->cpu0, ls->eip) &&
+		assert(!opcodes_are_atomic_swap(ls->instruction_text) &&
 		       "Data races on xchg/atomic instructions is unsupported "
 		       "-- see issue #88. Sorry!");
 		*data_race = true;
@@ -153,7 +153,7 @@ bool arbiter_interested(struct ls_state *ls, bool just_finished_reschedule,
 		unsigned int mutex_addr;
 		if (KERNEL_MEMORY(ls->eip)) {
 			return false;
-		} else if (instruction_is_atomic_swap(ls->cpu0, ls->eip) &&
+		} else if (opcodes_are_atomic_swap(ls->instruction_text) &&
 			   check_user_xchg(&ls->user_sync, ls->sched.cur_agent)) {
 			/* User thread is blocked on an "xchg-continue" mutex.
 			 * Analogous to HLT state -- need to preempt it. */

work/modules/landslide/landslide.c
@@ -91,7 +91,7 @@ struct ls_state *new_landslide()
 
 static void check_user_syscall(struct ls_state *ls)
 {
-	if (READ_BYTE(ls->cpu0, ls->eip) != OPCODE_INT) {
+	if (ls->instruction_text[0] != OPCODE_INT) {
 		ls->sched.cur_agent->most_recent_syscall = 0;
 		return;
 	}

work/modules/landslide/schedule.c
@@ -651,7 +651,7 @@ static void sched_update_kern_state_machine(struct ls_state *ls)
 				free_stack_trace(s->voluntary_resched_stack);
 			s->voluntary_resched_stack = stack_trace(ls);
 		}
-		if (READ_BYTE(ls->cpu0, ls->eip) == OPCODE_INT) {
+		if (ls->instruction_text[0] == OPCODE_INT) {
 			/* I'm not actually sure if this is where an INT would
 			 * happen in a hurdle-violationg context switcher...? */
 			HURDLE_VIOLATION("The context switch entry path seems "
@@ -664,7 +664,7 @@ static void sched_update_kern_state_machine(struct ls_state *ls)
 		lskprintf(DEV, "exiting context switch (TID %d)\n", CURRENT(s, tid));
 		/* the MZ memorial condition -- "some" context switchers might
 		 * also return from the timer interrupt handler. Attempt to cope. */
-		if (READ_BYTE(ls->cpu0, ls->eip) == OPCODE_IRET) {
+		if (ls->instruction_text[0] == OPCODE_IRET) {
 			if (!kern_timer_exiting(READ_STACK(ls->cpu0, 0))) {
 				ACTION(s, handling_timer) = false;
                                 lskprintf(DEV, "JFR site #1b\n");
@@ -898,7 +898,7 @@ static void sched_update_user_state_machine(struct ls_state *ls)
 			lockset_add(s, &CURRENT(s, user_locks_held), lock_addr, LOCK_MUTEX);
 		}
 		record_user_mutex_activity(&ls->user_sync);
-	} else if (user_yielding(ls->cpu0, ls->eip)) {
+	} else if (user_yielding(ls)) {
 		if (ACTION(s, user_mutex_locking)) {
 			/* "Probably" blocked on the mutex. */
 			assert(!ACTION(s, user_mutex_unlocking));

work/modules/landslide/user_specifics.h
@@ -11,6 +11,8 @@
 
 #include "student_specifics.h"
 
+struct ls_state;
+
 /* Userspace */
 
 bool testing_userspace();
@@ -18,7 +20,7 @@ bool user_within_functions(struct ls_state *ls);
 bool ignore_dr_function(unsigned int eip);
 /* syscalls / misc */
 bool user_report_end_fail(conf_object_t *cpu, unsigned int eip);
-bool user_yielding(conf_object_t *cpu, unsigned int eip);
+bool user_yielding(struct ls_state *ls);
 bool user_make_runnable_entering(unsigned int eip);
 bool user_sleep_entering(unsigned int eip);
 /* malloc */

work/modules/landslide/x86.c
@@ -343,17 +343,20 @@ char *read_string(conf_object_t *cpu, unsigned int addr)
 	return buf;
 }
 
-bool instruction_is_atomic_swap(conf_object_t *cpu, unsigned int eip) {
-	unsigned int op = READ_BYTE(cpu, eip);
-	if (op == 0xf0) {
+/* will read at most 3 opcodes */
+bool opcodes_are_atomic_swap(uint8_t *ops) {
+	unsigned int offset = 0;
+	if (ops[offset] == 0xf0) {
 		/* lock prefix */
-		return instruction_is_atomic_swap(cpu, eip + 1);
-	} else if (op == 0x86 || op == 0x87 || op == 0x90) {
+		offset++;
+	}
+
+	if (ops[offset] == 0x86 || ops[offset] == 0x87 || ops[offset] == 0x90) {
 		/* xchg */
 		return true;
-	} else if (op == 0x0f) {
-		unsigned int op2 = READ_BYTE(cpu, eip + 1);
-		if (op2 == 0xb0 || op2 == 0xb1) {
+	} else if (ops[offset] == 0x0f) {
+		offset++;
+		if (ops[offset] == 0xb0 || ops[offset] == 0xb1) {
 			/* cmpxchg */
 			return true;
 		} else {
@@ -362,6 +365,14 @@ bool instruction_is_atomic_swap(conf_object_t *cpu, unsigned int eip) {
 	} else {
 		return false;
 	}
+}
+
+bool instruction_is_atomic_swap(conf_object_t *cpu, unsigned int eip) {
+	uint8_t opcodes[3];
+	opcodes[0] = READ_BYTE(cpu, eip);
+	opcodes[1] = READ_BYTE(cpu, eip + 1);
+	opcodes[2] = READ_BYTE(cpu, eip + 2);
+	return opcodes_are_atomic_swap(opcodes);
 }
 
 /* a similar trick to avoid timer interrupt, but delays by just 1 instruction. */

work/modules/landslide/x86.h
@@ -62,7 +62,8 @@ bool interrupts_enabled(conf_object_t *cpu);
 unsigned int read_memory(conf_object_t *cpu, unsigned int addr, unsigned int width);
 bool write_memory(conf_object_t *cpu, unsigned int addr, unsigned int val, unsigned int width);
 char *read_string(conf_object_t *cpu, unsigned int eip);
-bool instruction_is_atomic_swap(conf_object_t *cpu, unsigned int eip);
+bool instruction_is_atomic_swap(conf_object_t *cpu, unsigned int eip); /* slower; uses READ_MEMORY */
+bool opcodes_are_atomic_swap(uint8_t *opcodes); /* faster; ok to use every instruction */
 unsigned int delay_instruction(conf_object_t *cpu);
 
 #define READ_BYTE(cpu, addr) \

commit 89f331dbaa437190c50098a4bec23def4767801f

Fix pebbles-only compile bug introduced in a0d9dd7b

work/modules/landslide/user_specifics.c
@@ -12,6 +12,7 @@
 #include "compiler.h"
 #include "kernel_specifics.h"
 #include "kspec.h"
+#include "landslide.h"
 #include "user_specifics.h"
 #include "x86.h"
 