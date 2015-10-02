d6a77455bee4e92f42d388f99d74cb90283d5efc
Ben Blum <bblum@andrew.cmu.edu>
2015-10-02 16:31:43 -0400

    [dr] Print 'PLDI time' in FAB job summary -- time since quicksand was started to begin with until bug was found

--- id/messaging.c
+++ id/messaging.c
@@ -400,6 +400,7 @@ void talk_to_child(struct messaging_state *state, struct job *j)
 				       "bug already found same job?");
 				j->trace_filename =
 					XSTRDUP(m.content.bug.trace_filename);
+				j->fab_timestamp = time_elapsed();
 				j->elapsed_branches++;
 				j->icb_fab_preemptions =
 					m.content.bug.icb_preemption_count;
