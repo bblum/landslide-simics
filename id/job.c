beee773bb9763015e4cae706a254b40183fbf726
Ben Blum <bblum@andrew.cmu.edu>
2015-11-02 20:32:57 -0500

    ignore is_runqueue cli/sti pps in pintos

--- id/job.c
+++ id/job.c
@@ -130,6 +130,7 @@ static void *run_job(void *arg)
 		/* basecode clis/stis */
 		XWRITE(&j->config_dynamic, "%s serial_putc\n", without);
 		XWRITE(&j->config_dynamic, "%s vga_putc\n", without);
+		XWRITE(&j->config_dynamic, "%s is_runqueue\n", without);
 	} else if (0 == strcmp(test_name, "mutex_test")) {
 		// XXX: Hack. This is special cased here, instead of being a
 		// cmdline option, so the studence don't have to worry about
